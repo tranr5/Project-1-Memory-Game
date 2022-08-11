7e03c0c704132724f00e65c49aad44ffde844f78
tranr5 <tranr4@gmail.com>
2022-08-11 13:45:23 -0400

    updated readme, timer

--- README.md
+++ README.md
@@ -8,13 +8,31 @@ Memory Game is a simple card game where you need to match pairs by turning over 
 
 JavaScript, CSS and HTML
 
-### Game Pictures
 
+### Rules of Memory Game
+<ul>
+<li>You will start by flipping over one card</li>
+<li>You flip two at a time, if there is a match, the cards will stay face up</li>
+<li>If the next card you flip does not match and the cards flip back</li>
+<li>The game continues until you match all the cards on the board or the timer runs out</li>
+</ul>
+
+### Game Pictures
+  When the game loads you will need to click the button to start the timer
+  <img src="https://i.imgur.com/GnSLwT7.png" height="300px"/> <br>
+  Whenever the player choose the cards to flip. If it is a match, they will stay flipped
+   <img src="https://i.imgur.com/XSaPwYQ.png" height="300px"/> 
+
+### Link to Game
+https://tranr5.github.io/Project-1-Memory-Game/
+
+
+Link to my wireframe<br>
+https://excalidraw.com/#json=CFRfuMzbAEJg2Mi4IzmVu,Hy7P68qPi_cFbYSUXGNOog
 
 ### Next Steps
-
-I would like to add a start screen before starting the game.  Create a fix for the double clicking in the game. Stop timer when you win the game
-
-
-Here is my link to my wireframe
-https://excalidraw.com/#json=CFRfuMzbAEJg2Mi4IzmVu,Hy7P68qPi_cFbYSUXGNOog
+<ul>
+<li>I would like to add a start screen before starting the game. </li>
+<li>Create a fix for the double clicking in the game. </li>
+<li>Stop timer when you win the game.</li>
+</ul>
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // flip...
 const cards = document.querySelectorAll('.grid-item');
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach(card => card.removeEventListener('click', flipCard));
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
@@ -30,31 +30,10 @@ function checkForMatch() {
         firstCard.classList.remove('grid-itemflip');
         secondCard.classList.remove('grid-itemflip');
     }, 500);
-    
     } 
-    
 }
 
-
-
-//checking if all cards are flipped
-// function allFlipped() {
-//     let count = 0
-//     for (let i = 0; i < cards.length+1; i++) {
-//         if () {
-//             count++
-//         console.log("gotem")
-//     } else {
-//         console.log("end Game here")
-
-//     }
-// }
-// }
-
-
-
 // shuffle effect that starts with an immediately envoked function    (func )();
-
 (function shuffleCards() {
     cards.forEach(card => {
         let j = Math.floor(Math.random() * 16);
@@ -63,14 +42,7 @@ function checkForMatch() {
 })();
 
 
-
-
-
-
-
-// i didn't want to make a reset button for the whole page, made one for the grid
-//NEED to add reset timer and also add a timer
-//it resets, but it doesnt' reshuffle nor can be clicked again
+// reset button for the grid
 let resetButton = document.getElementById("button");
 resetButton.addEventListener("click", () => {
     cards.forEach(card => {
@@ -78,7 +50,7 @@ resetButton.addEventListener("click", () => {
         //loops all cards, removing flip effect
         for (let i = 0; i < cards.length; i++) {
         card.classList.remove('grid-itemflip');
-                //shuffles with delay to fix seeing cards while being flipped
+        //shuffles with delay to fix seeing cards while being flipped
         setTimeout(() => {
             card.addEventListener('click', flipCard);
         let j = Math.floor(Math.random() * 16);
@@ -88,6 +60,21 @@ resetButton.addEventListener("click", () => {
 })});
 
 // timer, locks board when times up
+//reset function
+function resetTime() {
+    var timeleft = 40;
+var downloadTimer = setInterval( function() {
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "TIMES UP";
+    //locksboard
+    cards.forEach(card => card.removeEventListener('click', flipCard));
+  } else {
+    document.getElementById("countdown").innerHTML = "Time left "+timeleft;
+  }
+  timeleft -= 1;
+}, 1000);
+}
 
 // var timeleft = 40;
 // var downloadTimer = setInterval( function() {
@@ -102,26 +89,19 @@ resetButton.addEventListener("click", () => {
 //   timeleft -= 1;
 // }, 1000);
 
-
-//reset 
-function resetTime() {
-    var timeleft = 40;
-var downloadTimer = setInterval( function() {
-  if(timeleft <= 35){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "TIMES UP";
-    //locksboard
-    cards.forEach(card => card.removeEventListener('click', flipCard));
-  } else {
-    document.getElementById("countdown").innerHTML = "Time left "+timeleft;
-  }
-  timeleft -= 1;
-}, 1000);
-}
-
-
-// const flipCardsCount = document.querySelectorAll('.grid-item')
-// let count = 0
-//stop timer when I finish
-// function whenWin() {
     
+//work in progress
+//checking if all cards are flipped
+//stops timer when all cards are flipped
+// function allFlipped() {
+//     let count = 0
+//     for (let i = 0; i < cards.length+1; i++) {
+//         if () {
+//             count++
+//         console.log("gotem")
+//     } else {
+//         console.log("end Game here")
+
+//     }
+// }
+// }
